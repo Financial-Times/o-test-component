1fdcbe476a96c77c928e3c19e2c93ff64ed9b9d0
Lee Moody <lee@leemoody.co.uk>
2018-07-24 10:45:42 +0100

    Add constructor to module example.

--- src/js/example.js
+++ src/js/example.js
@@ -1,8 +1,9 @@
+/** @module example */
+
 /**
  * Test the JsDoc example tag.
  * http://usejsdoc.org/tags-example.html
  * Solves equations of the form a * x = b
- * @module example
  * @example
  * // returns 3
  * solveAsExample(5, 15);
@@ -11,7 +12,28 @@
  * solveAsExample(5, 10);
  * @returns {Number} Returns the value of x for the equation.
  */
-module.exports = function solveAsExample(a, b) {
+module.exports.solveAsExample = function solveAsExample(a, b) {
     return b / a;
 };
 
+/**
+ * An example constructor within a module.
+ * @constructor
+ * @example
+ * const myIncrementer = new incrementer();
+ * myIncrementer.increment(4); // 4
+ * myIncrementer.increment(4); // 8
+ * myIncrementer.increment(1); // 9
+ */
+module.exports = function incrementer() {
+    this.num = 0;
+    /**
+     * An example constructor within a module.
+     * @param {Number} num - The number to increment by.
+     * @returns {Number} - The updated value.
+     */
+    this.increment = function (num) {
+        this.num += num;
+        return this.num;
+    }
+};
