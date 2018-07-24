9e6af34c67768710ac4eb15af40ba5e81ec42dfb
Lee Moody <lee@leemoody.co.uk>
2018-07-24 11:24:51 +0100

    Add example of a single export module.

--- src/js/example.js
+++ src/js/example.js
@@ -1,4 +1,4 @@
-/** @module example */
+/** @module example-module */
 
 /**
  * Test the JsDoc example tag.
---
+++ src/js/module.js
@@ -0,0 +1,9 @@
+/**
+ * Tests a single export module.
+ * @module my-module
+ * @see module:example-module for an example of multi export module documentation.
+ * @returns {String} Returns a fixed string.
+ */
+module.exports = function () {
+    return 'I am a single export module.';
+};
