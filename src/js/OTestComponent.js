f810f321b7787f116dabac243d28e76f35d72b93
notlee <lee.moody@ft.com>
2018-09-27 15:33:00 +0100

    JS only component test

---
+++ src/js/OTestComponent.js
@@ -0,0 +1,13 @@
+/**
+ * A simple test class.
+ */
+class OTestComponent {
+    /**
+     * Say hello to the world.
+     */
+    static helloWorld() {
+        console.log('Hello world.');
+    }
+}
+
+export default OTestComponent;
--- src/js/simple-module.js
+++
@@ -1,9 +0,0 @@
-/**
- * Tests a single export module.
- * @module SimpleModule
- * @see module:example-module for an example of multi export module documentation.
- * @returns {String} Returns a fixed string.
- */
-module.exports = function () {
-	return 'I am a single export module.';
-};
