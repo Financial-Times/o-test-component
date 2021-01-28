1645833d291190fd72f47264967bb38542faecd8
notlee <notlee@users.noreply.github.com>
2021-01-28 11:21:38 +0000

    Update js/test

--- src/js/greet.js
+++ src/js/greet.js
@@ -1,11 +1,13 @@
 import { partial } from 'lodash-es';
 
 function greet(greeting, name) {
+	const phrase = greeting + ' ' + name;
 	// eslint-disable-next-line no-console
-	console.log(greeting + ' ' + name);
+	console.log(phrase);
+	return phrase;
 }
 
-const hello = partial(greet, 'Hello');
+const hello = partial(greet, 'hello');
 
 export { hello };
 export default greet;
--- test/greet.test.js
+++ test/greet.test.js
@@ -1,6 +1,6 @@
 /* eslint-env mocha */
 /* global proclaim */
-import hello from './../main.js';
+import { hello } from '../src/js/greet.js';
 
 describe("hello", () => {
 	it('says hello to the name given', () => {
